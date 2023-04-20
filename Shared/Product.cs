3e18214a82c8e7dbefd10a38fec9578030b0bb45
Drew Coker <drewrosecoker@gmail.com>
2023-04-19 22:47:26 -0400

    added comments

--- Shared/Product.cs
+++ Shared/Product.cs
@@ -5,12 +5,12 @@ namespace ShoppingCartApp.Shared
     public class Product
     {
         [JsonProperty("sku")]
-        public string? Sku { get; set; }
+        public string? Sku { get; set; }  // The SKU of the product
 
         [JsonProperty("title")]
-        public string? Title { get; set; }
+        public string? Title { get; set; }  // The title of the product
 
         [JsonProperty("price")]
-        public decimal Price { get; set; }
+        public decimal Price { get; set; }  // The price of the product
     }
 }
